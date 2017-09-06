05089d70f8e432a79ef7e69c304f36246b2e17d4
Jens <jenswehner@gmail.com>
2017-09-06 13:23:53 +0200

    made it more efficient

--- xtp/xtp_Expandjobfile.py
+++ xtp/xtp_Expandjobfile.py
@@ -12,41 +12,49 @@ args=parser.parse_args()
 
 
 def IntegrateJobfile(doneroot,openroot):
-    for entry1 in openroot.iter('job'):
-        inputs1=entry1.find("input")
-        status1=entry1.find("status")
-        if status1.text=="COMPLETE":
-	    continue
-        for i,j in enumerate(inputs1.iter("segment")):
-            if i==0:
-                seg1_1=(int(j.text))
-            elif i==1:
-                seg2_1=(int(j.text))
-            else:
-                print "More than two segments in input"
-        
-        for entry2 in doneroot.iter("job"):
-            inputs2=entry2.find("input")
-            status2=entry2.find("status")
-            if status2.text!="COMPLETE":
-	        continue
-            for i,j in enumerate(inputs2.iter("segment")):
-                if i==0:
-                    seg1_2=(int(j.text))
-                elif i==1:
-                    seg2_2=(int(j.text))
-                else:
-                    print "More than two segments in input"
-            if seg1_2==seg1_1 and seg2_1==seg2_2:
-                output=entry2.find("output")
-                host=entry2.find("host")
-                
-                time=entry2.find("time")
-                status1.text="COMPLETE"
-                entry1.append(host)
-                entry1.append(time)
-                entry1.append(output)
-                break
+	used=[]
+	for entry2 in doneroot.iter("job"):
+		used.append(False)
+	
+
+	for entry1 in openroot.iter('job'):
+		inputs1=entry1.find("input")
+		status1=entry1.find("status")
+		if status1.text=="COMPLETE":
+			continue
+		for i,j in enumerate(inputs1.iter("segment")):
+			if i==0:
+				seg1_1=(int(j.text))
+			elif i==1:
+				seg2_1=(int(j.text))
+			else:
+				print "More than two segments in input"
+		
+		for index,entry2 in enumerate(doneroot.iter("job")):
+			if used[index]:
+				continue
+			inputs2=entry2.find("input")
+			status2=entry2.find("status")
+			if status2.text!="COMPLETE":
+				continue
+			for i,j in enumerate(inputs2.iter("segment")):
+				if i==0:
+					seg1_2=(int(j.text))
+				elif i==1:
+					seg2_2=(int(j.text))
+				else:
+					print "More than two segments in input"
+			if seg1_2==seg1_1 and seg2_1==seg2_2:
+				used[index]=True
+				output=entry2.find("output")
+				host=entry2.find("host")
+				
+				time=entry2.find("time")
+				status1.text="COMPLETE"
+				entry1.append(host)
+				entry1.append(time)
+				entry1.append(output)
+				break
 
 
 # In[3]:
