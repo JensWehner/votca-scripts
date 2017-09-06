f8aec02fb711e21cedc0707d1ef051b9b9345fab
Jens <jenswehner@gmail.com>
2017-09-06 10:26:02 +0200

    new script

---
+++ xtp/xtp_Expandjobfile.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+from __tools__ import *
+import argparse as ap
+import numpy.linalg as lg
+
+
+parser=ap.ArgumentParser(description="Parsing two jobfiles and merging one into the other")
+parser.add_argument("-j1","--jobfile1",required=True, help="jobfile to expand")
+parser.add_argument("-j2","--jobfile2",required=True, help="jobfile to integrate")
+args=parser.parse_args()
+
+# In[2]:
+
+
+def IntegrateJobfile(doneroot,openroot):
+    for entry1 in openroot.iter('job'):
+        inputs1=entry1.find("input")
+        status=entry1.find("status")
+        for i,j in enumerate(inputs1.iter("segment")):
+            if i==0:
+                seg1_1=(int(j.text))
+            elif i==1:
+                seg2_1=(int(j.text))
+            else:
+                print "More than two segments in input"
+        
+        for entry2 in doneroot.iter("job"):
+            inputs2=entry2.find("input")
+            for i,j in enumerate(inputs2.iter("segment")):
+                if i==0:
+                    seg1_2=(int(j.text))
+                elif i==1:
+                    seg2_2=(int(j.text))
+                else:
+                    print "More than two segments in input"
+            if seg1_2==seg1_1 and seg2_1==seg2_2:
+                output=entry2.find("output")
+                host=entry2.find("host")
+                
+                time=entry2.find("time")
+                status.text="COMPLETE"
+                entry1.append(host)
+                entry1.append(time)
+                entry1.append(output)
+                break
+
+
+# In[3]:
+
+
+doneroot=XmlParser(args.jobfile2)
+openroot=XmlParser(args.jobfile1)
+
+
+# In[4]:
+
+
+IntegrateJobfile(doneroot,openroot)
+
+
+# In[5]:
+
+
+XmlWriter(openroot,args.jobfile1)
+
+
+# In[ ]:
+
+
+
+
